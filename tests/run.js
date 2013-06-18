927320fed6da3557e198f89fb2f9bccd0b7e6ff0
Alex Kocharin <alex@kocharin.ru>
2013-06-18 06:28:02 +0400

    tests fix

--- tests/run.js
+++ tests/run.js
@@ -5,9 +5,10 @@ var editor = require('..').edit;
 var assert = require('assert');
 var fs = require('fs');
 
-fs.readdirSync('resources').forEach(function(file) {
+fs.readdirSync(__dirname + '/resources').forEach(function(file) {
+   if (!file.match(/\.yaml$/)) return;
 	console.log('running', file);
-	var data = fs.readFileSync('yaml-editor/'+file, 'utf8').split('---\n');
+	var data = fs.readFileSync(__dirname + '/resources/'+file, 'utf8').split('---\n');
 	assert(data.length === 2);
 	editor(data[0], JSON.stringify(yaml.safeLoad(data[1])), data[1]);
 });
