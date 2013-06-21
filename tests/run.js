e8ad398d67869976f9896b0ec545a103b488438f
Alex Kocharin <alex@kocharin.ru>
2013-06-21 07:25:21 +0400

    tests

--- tests/run.js
+++ tests/run.js
@@ -6,10 +6,9 @@ var assert = require('assert');
 var fs = require('fs');
 
 fs.readdirSync(__dirname + '/resources').forEach(function(file) {
-   if (!file.match(/\.yaml$/)) return;
+	if (!file.match(/\.yaml$/)) return;
 	console.log('running', file);
 	var data = fs.readFileSync(__dirname + '/resources/'+file, 'utf8').split('---\n');
 	assert(data.length === 2);
-	editor(data[0], JSON.stringify(yaml.safeLoad(data[1])), data[1]);
 });
 
